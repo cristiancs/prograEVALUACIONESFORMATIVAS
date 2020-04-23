e8bf89492f28915a732a0aeafde32020c1497329
Cristian Navarrete <cristiannavarrete@outlook.com>
2020-04-23 16:28:22 -0400

    Print solo de la nota para copiar y pegar.

--- procesar.py
+++ procesar.py
@@ -45,14 +45,14 @@ if len(sys.argv) > 1:
     Path("./SALIDA/"+curso).mkdir(parents=False, exist_ok=True)
     for filename in all_files:
         paralelo = filename.split("_")[2]
-        print("PARALELO: " + paralelo)
+        print("\nPARALELO: " + paralelo)
         lista_curso = pd.read_excel(filename, skiprows=8, dtype=str)
         lista_curso["rut"] = lista_curso["RUT"] + "-" + lista_curso["DV.1"]
         lista_curso = lista_curso.merge(
             df, left_on='rut', right_on='rut', how='left')
 
         lista_curso.to_excel("SALIDA/"+curso+"/"+paralelo+".xls")
-        print(lista_curso)
+        print(lista_curso['nota'].to_string(index=False))
 
 
 else:
