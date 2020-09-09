732b18497fe0f7b66a04397b5fc03ae4e969bbc6
Cristian Navarrete <cristiannavarrete@outlook.com>
2020-09-09 14:53:51 -0300

    actualizar script para que calcule el promedio de las evaluaciones y use el email como llave en el merge (debido a al gran cantidad de alumnos que no tienen el rut en aula).

--- procesar.py
+++ procesar.py
@@ -7,8 +7,6 @@ from pathlib import Path
 # Archivo Base
 # Descarga desde https://aula.usm.cl/grade/export/txt/index.php?id=107441
 
-fields = [2, 6]
-
 
 curso = sys.argv[1]
 
@@ -29,11 +27,12 @@ try:
     i = 0
 
     for x in columnas_originales:
-        if i in [0, 1,  3, 4, 5, len(columnas_originales)-1]:
+        if i in [0, 1, 2, 3, 4, len(columnas_originales)-1]:
             df.drop([x], axis=1, inplace=True)
         i += 1
-    df.rename(columns={"Número de ID": "rut"}, inplace=True)
-    df["rut"] = df["rut"].str.upper()
+   # df.rename(columns={"Número de ID": "rut"}, inplace=True)
+    df.rename(columns={"Dirección de correo": "correo"}, inplace=True)
+    #df["rut"] = df["rut"].str.upper()
 except ValueError as e:
     print("No hay archivos en la carpeta ENTRADA")
     exit(1)
@@ -48,24 +47,24 @@ columnas_originales = df.columns[1:]
 
 
 for x in columnas_originales:
-    df.loc[df[x] != '-', x] = 1
+    df.loc[df[x] != '-', x] = df[x]
     df.loc[df[x] == '-', x] = 0
     df.rename(columns={x: "cont_"+str(i)}, inplace=True)
-    df = df.astype({"cont_"+str(i): "int64"})
+    df = df.astype({"cont_"+str(i): "float32"})
     columnas_aux.append("cont_"+str(i))
     i += 1
 
 
-df["nota"] = df.sum(axis=1)
-
+df["nota"] = df.mean(axis=1) * 10
+df = df.astype({"nota": "int64"})
 df.drop(columnas_aux, axis=1, inplace=True)
 
 
-df.loc[df.nota == cant_evaluaciones, "nota"] = 100
+#df.loc[df.nota == cant_evaluaciones, "nota"] = 100
 # Valor temporal para que no les de un 1 cuando es un 0
-df.loc[df.nota == 0, "nota"] = cant_evaluaciones+5
-df.loc[df.nota < cant_evaluaciones, "nota"] = 1
-df.loc[df.nota == cant_evaluaciones+5, "nota"] = 0
+#df.loc[df.nota == 0, "nota"] = cant_evaluaciones+5
+#df.loc[df.nota < cant_evaluaciones, "nota"] = 1
+#df.loc[df.nota == cant_evaluaciones+5, "nota"] = 0
 
 
 # Las pasamos a la lista de cada curso
@@ -85,9 +84,10 @@ for filename in all_files:
     paralelo = filename.split("_")[2]
     print("\nPARALELO: " + paralelo)
     lista_curso = pd.read_excel(filename, skiprows=8, dtype=str)
-    lista_curso["rut"] = lista_curso["RUT"] + "-" + lista_curso["DV.1"]
+
+    #lista_curso["rut"] = lista_curso["RUT"] + "-" + lista_curso["DV.1"]
     lista_curso = lista_curso.merge(
-        df, left_on='rut', right_on='rut', how='left')
+        df, left_on='Correo', right_on='correo', how='left')
 
     lista_curso.to_excel(writer, sheet_name=paralelo)
     # print(lista_curso['nota'].to_string(index=False))
